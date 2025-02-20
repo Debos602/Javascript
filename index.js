ca02333981e94cb9818b4b630a55cc3792ca2713
unknown <debos.das.02@gmail.com>
2025-02-20 17:33:48 +0600

    javascript call-stack function

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("Hello World");
+// javascript is single threaded
+// javascript is synchronous
+// javascript is dynamic
+// javascript is weakly typed
+// javascript
+// It is known as  call stack, Execution contact stack, Program Stack, Control stack, Execution stack, Run-time stack, Machine stack
+// It is a data structure which record the function call
+// It is a stack data structure
+// It is a LIFO data structure
+// It is a stack of frames
+// It is a stack of function call
+// It is a stack of execution context
+// It is a stack of execution context object
+// It is a stack of lexical environment
+var n = 2;
+function squere(ab) {
+    var ans = ab * ab;
+    return ans;
+}
+console.log(squere(2));
+console.log(squere(3));
+console.log(squere(5));
